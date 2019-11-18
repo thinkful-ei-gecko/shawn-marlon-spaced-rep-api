3f79210615edf4d96f6c39ed852db73ad1643e8e
Shawn Collette <shawn.collette@gmail.com>
2019-11-18 11:03:47 -0700

    postgres automation and seed file update

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,11 +1,20 @@
 require('dotenv').config();
 
+require('dotenv').config();
+
 module.exports = {
-  "migrationDirectory": "migrations",
+  "migrationsDirectory": "migrations",
   "driver": "pg",
-  "host": process.env.MIGRATION_DB_HOST,
-  "port": process.env.MIGRATION_DB_PORT,
-  "database": process.env.MIGRATION_DB_NAME,
-  "username": process.env.MIGRATION_DB_USER,
-  "password": process.env.MIGRATION_DB_PASS
+  "connectionString": process.env.DB_URL,
 }
+
+
+// module.exports = {
+//   "migrationDirectory": "migrations",
+//   "driver": "pg",
+//   "host": process.env.MIGRATION_DB_HOST,
+//   "port": process.env.MIGRATION_DB_PORT,
+//   "database": process.env.MIGRATION_DB_NAME,
+//   "username": process.env.MIGRATION_DB_USER,
+//   "password": process.env.MIGRATION_DB_PASS
+// }
