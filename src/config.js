20d1a64672cf36042ad22ec9dc6be6ec35daa4a8
Shawn Collette <shawn.collette@gmail.com>
2019-11-22 21:30:28 -0700

    removed CLIENT_ORIGIN pointer

--- src/config.js
+++ src/config.js
@@ -1,9 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL: process.env.DB_URL,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  PORT: process.env.PORT || 8080,
+  DB_URL: process.env.DB_URL || "postgresql://admin@localhost/morse",
   JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
   JWT_EXPIRY: process.env.JWT_EXPIRY || "20m"
 };
